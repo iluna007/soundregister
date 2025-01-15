f7f404f786df5d1f8e3d114a54b5704a2631814d
iluna007 <146373472+iluna007@users.noreply.github.com>
2025-01-15 22:23:45 +0000

    Ultimo arreglo

--- backend/models.py
+++ backend/models.py
@@ -5,18 +5,15 @@ from sqlalchemy import Float
 from datetime import datetime
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    role = db.Column(db.String(80), nullable=False, default='user')
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "username": self.username,
-            "email": self.email
-        }
+    def __repr__(self):
+        return f'<User {self.username}>'
         
 
 #### audio ---------------
